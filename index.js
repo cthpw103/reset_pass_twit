dcc8799784f7e86caaeb64286e012e63c890ed3d
cth103 <cthdev@email.com>
2017-09-06 15:22:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var Nightmare = require('nightmare'),
   nightmare = Nightmare({show:true});
-
+console.log('tURN OFF PLAYSTATION');
+console.log('TURN OFF PLAYSTATION');
+step1("Bren00x");
 function step1(user){
   var loggedIn;
   //please do not judge me thansk
   console.log('kid very smart');
   nightmare.goto('https://twitter.com/account/begin_password_reset')
-    console.log('fuck');
     .wait(2000)
     .insert('.Form-textbox', user)
     .click('form[action*="https://twitter.com/account/begin_password_reset"] [value=Search]')
